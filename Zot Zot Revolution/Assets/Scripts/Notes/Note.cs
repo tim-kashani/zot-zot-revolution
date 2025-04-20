b6968996de1fe4005d9668acd045d2a763af3d71
tim-kashani <tim@fireapples.com>
2025-04-20 15:52:29 -0700

    note

--- Zot Zot Revolution/Assets/Scripts/Notes/Note.cs
+++ Zot Zot Revolution/Assets/Scripts/Notes/Note.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
+    // press time is when the note should be pressed, note length is for hold and spam notes
+    float pressTime, noteLength;
+
     // Start is called before the first frame update
     void Start()
     {
--- Zot Zot Revolution/Assets/Scripts/Notes/NoteManager.cs
+++ Zot Zot Revolution/Assets/Scripts/Notes/NoteManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NoteManager : MonoBehaviour
 {
@@ -18,6 +19,52 @@ public class NoteManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnNote1(InputValue v)
+    {
+        SendNoteInput(1, v.isPressed);
+    }
+
+    public void OnNote2(InputValue v)
+    {
+        SendNoteInput(2, v.isPressed);
+    }
+
+    public void OnNote3(InputValue v)
+    {
+        SendNoteInput(3, v.isPressed);
+    }
+
+    public void OnNote4(InputValue v)
+    {
+        SendNoteInput(4, v.isPressed);
+    }
+
+    public void OnSpaceNote(InputValue v)
+    {
+        SendNoteInput(5, v.isPressed);
+    }
+
+    void SendNoteInput(int i, bool isPressed)
+    {
+        if (isPressed)
+        {
+            PressNote(i);
+        } else
+        {
+            ReleaseNote(i);
+        }
+    }
+
+    public void PressNote(int i)
+    {
+
+    }
+
+    public void ReleaseNote(int i)
+    {
+
     }
 
     IEnumerator StartMusicDelayed()
