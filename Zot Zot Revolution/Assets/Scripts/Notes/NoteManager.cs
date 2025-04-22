ef461a0789f5f282b1c68c23a590228e93fdc2b8
tim-kashani <tim@fireapples.com>
2025-04-22 16:23:42 -0700

    Update NoteManager.cs

--- Zot Zot Revolution/Assets/Scripts/Notes/NoteManager.cs
+++ Zot Zot Revolution/Assets/Scripts/Notes/NoteManager.cs
@@ -69,7 +69,7 @@ public class NoteManager : MonoBehaviour
                 noteXPosition = 5;
             }
 
-            note.SetXPosition(noteXPosition);
+            note.SetXPositionAndTime(noteXPosition, notes[i].x);
         }
     }
 
