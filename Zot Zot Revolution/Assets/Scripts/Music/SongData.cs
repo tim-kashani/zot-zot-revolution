213a314904135d5794057212db300acbb6e2232c
tim-kashani <tim@fireapples.com>
2025-05-08 15:50:52 -0700

    Update SongData.cs

--- Zot Zot Revolution/Assets/Scripts/Music/SongData.cs
+++ Zot Zot Revolution/Assets/Scripts/Music/SongData.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SongData : MonoBehaviour
+[CreateAssetMenu]
+public class SongData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string songName;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string composerName;
+
+    public string characterArtistName;
+
+    public AudioClip song;
+
+    public string midiFilePath;
 }
