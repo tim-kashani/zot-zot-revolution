1376bc009b5d81f8f1bd63bed3f5b258db76c767
tim-kashani <tim@fireapples.com>
2025-05-27 22:07:46 -0700

    hmm

--- Zot Zot Revolution/Assets/Scripts/Misc/Visualizer.cs
+++ Zot Zot Revolution/Assets/Scripts/Misc/Visualizer.cs
@@ -8,22 +8,39 @@ public class Visualizer : MonoBehaviour
 
     [SerializeField] LineRenderer lineRenderer;
 
+    float[] samples;
+
+    Vector2[] positions;
+
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer.positionCount = sampleSize;
 
+        positions = new Vector2[sampleSize];
+
         for (int i = 0; i < lineRenderer.positionCount; i++)
         {
             float f = Mathf.PI * 2 * i / sampleSize;
 
-            lineRenderer.SetPosition(i, new(Mathf.Sin(f), Mathf.Cos(f), 0));
+            positions[i] = new(Mathf.Sin(f), Mathf.Cos(f));
+
+            lineRenderer.SetPosition(i, positions[i]);
         }
+
+        samples = new float[sampleSize];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AudioListener.GetOutputData(samples, 0);
+
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            float f = 1 - samples[i];
+
+            lineRenderer.SetPosition(i, f * positions[i]);
+        }
     }
 }
