a1c6f7996f44519ef4a179e0b11aafb0e941952c
tim-kashani <tim@fireapples.com>
2025-05-27 21:10:33 -0700

    oops

--- Zot Zot Revolution/Assets/Scripts/Notes/NoteManager.cs
+++ Zot Zot Revolution/Assets/Scripts/Notes/NoteManager.cs
@@ -335,7 +335,7 @@ public class NoteManager : MonoBehaviour
             return;
         }
 
-        if (note.GetPressTime() - music.GetCurrentBeat() <= -0.5f)
+        if (note.GetPressTime() - music.GetCurrentBeat() > 0.5f)
         {
             return;
         }
